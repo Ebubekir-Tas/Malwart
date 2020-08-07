4474ebea733a76d5ceabe3538851b2d2755a9c70
Ebubekir Tas <Tas_Ebubekir@yahoo.com>
2020-08-06 22:55:10 -0400

    responsiveness update + favicon

---
+++ index.js
@@ -0,0 +1,14 @@
+$(".mal-title1").click(function(){
+    $(".block-bottom1").slideToggle("slow", function() {
+      });
+});
+
+$(".mal-title2").click(function(){
+    $(".block-bottom2").slideToggle("slow", function() {
+      });
+    });
+
+$(".mal-title3").click(function(){
+    $(".block-bottom3").slideToggle("slow", function() {
+        });
+    });
